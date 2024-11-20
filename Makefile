79e9a917da0eb5615d59c0b7ee9806fc41a502fb
Triomphant18 <triomphant.suanon@epitech.eu>
2024-11-20 12:47:18 +0100

    feat: download and embedding images

---
+++ Makefile
@@ -0,0 +1,66 @@
+# Variables
+ML_CONFIG_PATH = training/config.yaml
+
+# Build and run docker env
+docker_up:
+	@echo "Build and run docker env..."
+	docker-compose up --build
+
+# Stop containers
+docker_down:
+	@echo "Stop containers..."
+	docker-compose down
+
+# Visualize docker logs
+docker_logs:
+	docker-compose logs -f
+
+# Install dependencies with Poetry
+install_deps:
+	@echo "Installing dependencies with Poetry..."
+	poetry install
+	poetry run python -m spacy download en_core_web_sm --config $(ML_CONFIG_PATH)
+
+# Load data from raw database
+load_data:
+	@echo "Load raw data..."
+	poetry run python training/main.py --load_data --config $(ML_CONFIG_PATH)
+
+# Run MLFlow
+mlflow:
+	@echo "Run MLFlow..."
+	poetry run python training/main.py --mlflow --config $(ML_CONFIG_PATH)
+
+# Run the main machine learning pipeline
+run:
+	@echo "Running the main project pipeline..."
+	@bash ./secrets.sh
+	$(MAKE) load_data
+	$(MAKE) mlflow
+
+# Download all the images
+download_images:
+	@echo "Downloading all the images..."
+	poetry run python training/images_downloader.py --config $(ML_CONFIG_PATH)
+
+# Clean temporary and cache files
+clean:
+	@echo "Cleaning up temporary files..."
+	rm -rf __pycache__ */__pycache__
+	find . -type f -name '*.pyc' -delete
+	find . -type d -name '__pycache__' -delete
+
+# Run all tests in the project with Poetry
+run_tests:
+	@echo "Running tests..."
+	poetry run python -m unittest discover -s tests/api
+
+	poetry run python -m unittest discover -s tests/training
+
+# Display available Makefile commands
+help:
+	@echo "Available Makefile commands:"
+	@echo "  make install_deps    - Install dependencies with Poetry"
+	@echo "  make run             - Run the main machine learning pipeline"
+	@echo "  make clean           - Remove temporary and cache files"
+	@echo "  make run_tests       - Run unit and integration tests"
---
+++ training/image_prepocessing.py
@@ -0,0 +1,118 @@
+from utils.config import load_config
+from utils.logger import setup_logger
+
+import os
+import pandas as pd
+from tflite_support.task import vision
+import io
+from PIL import Image
+import numpy as np
+
+import kagglehub
+
+logger = setup_logger()
+
+# Download latest version
+path = kagglehub.model_download(
+    "google/mobilenet-v3/tfLite/small-100-224-feature-vector-metadata"
+)
+path = path + "/1.tflite"
+print("Path to model files:", path)
+
+
+# Initialise le modèle d'embedder pour les images
+image_embedder = vision.ImageEmbedder.create_from_file(path)
+
+def get_image_embedding(image_path):
+    """
+    Génère un embedding pour une image locale en utilisant ImageEmbedder.
+
+    Parameters:
+        image_path (str): Le chemin local de l'image.
+
+    Returns:
+        np.ndarray: Le vecteur de caractéristiques de l'image.
+    """
+    try:
+        image = Image.open(image_path)
+        image_array = np.array(image)
+
+        # Extraction de l'embedding
+        tensor_image = vision.TensorImage.create_from_array(image_array)
+        embedding_result = image_embedder.embed(tensor_image)
+        feature_vector = embedding_result.embeddings[0].feature_vector
+        return feature_vector
+    except Exception as e:
+        # print(f"Erreur lors du traitement de l'image {image_path}: {e}")
+        return None
+
+
+def get_all_image_embeddings(df_path, code_column, save_dir):
+    """
+    Génère des embeddings pour toutes les images déjà téléchargées localement.
+
+    Parameters:
+        df (DataFrame): Le DataFrame contenant les codes uniques pour retrouver les images.
+        code_column (str): Le nom de la colonne contenant les codes uniques.
+        save_dir (str): Le dossier où sont enregistrées les images.
+
+    Returns:
+        DataFrame: Un DataFrame contenant les embeddings de chaque image dans une colonne `image_embedding`.
+    """
+    df = pd.read_csv(df_path)
+    embeddings = []
+    embeddings_array = []
+    img_download = 25000
+
+    for index, row in df.iterrows():
+        code = row[code_column]
+        image_path = os.path.join(save_dir, f"{code}.jpg")
+
+        if os.path.exists(image_path):
+            # Génère l'embedding si l'image existe
+            embedding = get_image_embedding(image_path)
+            if embedding:
+                embedding_array = embedding.value
+            else:
+                embedding_array = None
+        else:
+            # print(f"L'image avec le code {code} est manquante dans {save_dir}.")
+            embedding = None
+            embedding_array = None
+
+        if index == img_download:
+            print(f"{img_download} images traitées sur {len(df)}")
+            img_download += 25000
+
+        embeddings.append(embedding)
+        embeddings_array.append(embedding_array)
+
+    # Ajoute les embeddings au DataFrame
+    df["image_embedding"] = embeddings
+    df["embedding_array"] = embeddings_array
+    df.to_csv("data/clean_datase_with_embeddings.csv")
+    return df
+
+
+def main(config_path: str):
+    # Load configuration from file
+    config = load_config(config_path)
+
+    df_path = config["data"]["clean_data_path"]
+    code_column = config["data"]["code_column"]
+    save_dir = config["data"]["images_path"]
+
+    get_all_image_embeddings(df_path, code_column=code_column, save_dir=save_dir,)
+
+
+if __name__ == "__main__":
+    logger.info("Embedding start")
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Téléchargement d'images")
+    parser.add_argument(
+        "--config", required=True, help="Chemin du fichier de configuration"
+    )
+    args = parser.parse_args()
+
+    main(args.config)
